6b27f80571529f15543a2ed4cbed57cea6c69882
¨kubraaygun¨ <kubraaygun@outlook.com.tr>
2025-04-23 00:46:48 +0300

    Setup navigation system

--- .history/App_20250423003113.tsx
+++ .history/App_20250423003113.tsx
@@ -2,16 +2,21 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {StyleSheet} from 'react-native';
-import MainRouter from './src/router/mainRouter';
 
 // create a component
 const App: React.FC = () => {
-  return (
-    <NavigationContainer>
-      <MainRouter />
-    </NavigationContainer>
-  );
+  return <NavigationContainer></NavigationContainer>;
 };
+
+// define your styles
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#2c3e50',
+  },
+});
 
 //make this component available to the app
 export default App;
---
+++ .history/src/router/mainRouter_20250423003328.tsx
@@ -0,0 +1,16 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+//import liraries
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const Stack = createNativeStackNavigator();
+const MainRouter: React.FC = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Users" component={Users} />
+    </Stack.Navigator>
+  );
+};
+
+export default MainRouter;
